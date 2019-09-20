c479dc1f8cd048f41d3a4190684d7bfc517f4da6
Tainan Santos <tainan206@gmail.com>
2019-09-20 13:01:21 -0300

    telas de Cadastro Curriculo

--- src/projeto_pbd/com/br/view/CadastroCurriculo.java
+++ src/projeto_pbd/com/br/view/CadastroCurriculo.java
@@ -1,4 +1,59 @@
 package projeto_pbd.com.br.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class CadastroCurriculo {
+
+    @FXML
+    private GridPane paneCadastroCurriculoUm;
+    @FXML
+    private AnchorPane paneCadastroCurriculoDois;
+    @FXML
+    private AnchorPane paneCadastroCurriculoTres;
+
+    public void proximo(){
+
+        // FAZER VALIDAÇÃO DOS COMAPOS ANTES
+
+        trocarTelas(2);
+    }
+
+    public void anterior(){
+        trocarTelas(1);
+    }
+
+
+    public void proximoDois(){
+        trocarTelas(3);
+    }
+
+
+    public void anteriorDois(){
+        trocarTelas(2);
+    }
+
+
+    public void salvarCurriculo(){
+
+    }
+
+
+    public void trocarTelas(int valor){
+        this.paneCadastroCurriculoUm.setVisible(false);
+        this.paneCadastroCurriculoDois.setVisible(false);
+        this.paneCadastroCurriculoTres.setVisible(false);
+        switch (valor) {
+            case 1:
+                this.paneCadastroCurriculoUm.setVisible(true);
+                break;
+            case 2:
+                this.paneCadastroCurriculoDois.setVisible(true);
+                break;
+            case 3:
+                this.paneCadastroCurriculoTres.setVisible(true);
+                break;
+        }
+    }
 }
